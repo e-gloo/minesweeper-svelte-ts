b3fb609b49a6b954a7ba2fef7b25b795ff7709ef
e-gloo <dylan.coodien@epitech.eu>
2022-07-24 21:10:31 +0200

    Project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      "#lib": path.resolve(__dirname, 'src/lib'),
+      "#store": path.resolve(__dirname, 'src/store'),
+      "#types": path.resolve(__dirname, 'src/types'),
+      "#utils": path.resolve(__dirname, 'src/utils'),
+    },
+  },
+});
